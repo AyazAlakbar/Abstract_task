3bfed92824af6ae388e99732303bd4e7bed9b7df
alexberg90 <ayazya@code.edu.az>
2022-12-20 17:20:33 +0400

    Abstract

---
+++ ASbstract/Class1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ASbstract
+{
+    abstract class Transportation
+    {
+        public abstract void Air();
+
+
+        
+    }
+     class Plane : Transportation
+    {
+        public override void Air()
+        {
+            Console.WriteLine("fly");
+        }
+
+        
+    }
+
+
+
+
+}
---
+++ ASbstract/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ASbstract
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+
+            Plane plane = new Plane();
+            plane.Air();
+
+        }
+    }
+}
